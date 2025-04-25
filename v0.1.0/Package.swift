992b59e03901572a65820bebd518e51e139f9778
Bo Gosmer <bo@bookbites.com>
2025-04-25 14:57:23 +0200

    Very first alpha version of the SDK to get the SPM hosting in GitHub working and tested.

---
+++ v0.1.0/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "WeDoBooksSDK",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "WeDoBooksSDK",
+            targets: ["WeDoBooksSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WeDoBooksSDK",
+            url: "https://github.com/wedobooks/wedobooks-sdk-ios-spm/v0.1.0/WeDoBooksSDK-v0.1.0.xcframework.zip",
+            checksum: "e31fb72f186b965d26aff0a5be27d75ebddfeb89abc21c103efc45386de16651"
+        )
+    ]
+)
