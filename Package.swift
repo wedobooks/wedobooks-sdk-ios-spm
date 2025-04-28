9c1d60d4b2c2056d398ef69b692e585043adbec6
Bo Gosmer <bo@bookbites.com>
2025-04-28 08:10:51 +0200

    Not using "v" in release tag

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeDoBooksSDK",
-            url: "https://github.com/wedobooks/wedobooks-sdk-ios-spm/releases/download/v0.1.0/WeDoBooksSDK-v0.1.0.xcframework.zip",
+            url: "https://github.com/wedobooks/wedobooks-sdk-ios-spm/releases/download/0.1.0/WeDoBooksSDK-v0.1.0.xcframework.zip",
             checksum: "e31fb72f186b965d26aff0a5be27d75ebddfeb89abc21c103efc45386de16651"
         )
     ]
