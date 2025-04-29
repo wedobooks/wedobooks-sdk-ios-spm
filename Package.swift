f9731c9bbae05c38895ae931e31bb7f506ed873c
Bo Gosmer <bo.gosmer@wedobooks.io>
2025-04-29 12:49:13 +0200

    Updated URL to point to the minimal cloud run service

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeDoBooksSDK",
-            url: "https://storage.cloud.google.com/wdb-ios-sdk/0.1.0/WeDoBooksSDK-v0.1.0.xcframework.zip",
+            url: "https://wdb-ios-spm-844218222632.europe-west1.run.app/WeDoBooksSDK-v0.1.0.xcframework.zip",
             checksum: "e31fb72f186b965d26aff0a5be27d75ebddfeb89abc21c103efc45386de16651"
         )
     ]
