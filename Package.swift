18cf1728e4e32d244fdd00e97fad64a1001418ff
Bo Gosmer <bo.gosmer@wedobooks.io>
2025-06-12 17:28:34 +0200

    Updating to v0.1.4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WeDoBooksSDK",
-            url: "https://wdb-ios-spm-844218222632.europe-west1.run.app/WeDoBooksSDK-v0.1.3.xcframework.zip",
-            checksum: "db062f2171797482cc5410a1422bba0514e1fb6064471729ecf4e79bf244d84f"
+            url: "https://wdb-ios-spm-844218222632.europe-west1.run.app/WeDoBooksSDK-v0.1.4.xcframework.zip",
+            checksum: "05d86d10825ba11911dbf80409d23f1763b40b42705dd3764698c022dca8594b"
         )
     ]
 )
