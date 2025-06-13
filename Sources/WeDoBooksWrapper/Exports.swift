5faadbc49ac16df277a84146551cdbff737a6cca
Bo Gosmer <bo.gosmer@wedobooks.io>
2025-06-13 14:00:05 +0200

    Trying to hide ColibrioReader from being imported by consumers of the SDK

--- Sources/WeDoBooksWrapper/Exports.swift
+++ Sources/WeDoBooksWrapper/Exports.swift
@@ -1,4 +1,5 @@
-// Re-export of public WeDoBooksSDK symbols
-// NOTE: @_exported is an internal Swift attribute but widely used in SPM modules.
+// Re-export of public WeDoBooksSDK symbols and hiding of ColibrioReader
+// NOTE: @_exported and @_implementationOnly are internal Swift attributes but widely used in SPM modules.
 // If it becomes deprecated, replace with manual wrapper API.
 @_exported import WeDoBooksSDK
+@_implementationOnly import ColibrioReader
