32f91f5e0a1e64ec82ec93d5d17ce7cfd7003788
Bo Gosmer <bo.gosmer@wedobooks.io>
2025-06-13 12:38:57 +0200

    Migrate to wrapper style Package.swift with ColibrioReader included

--- Package.swift
+++ Package.swift
@@ -8,15 +8,27 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "WeDoBooksSDK",
-            targets: ["WeDoBooksSDK"]
+            name: "WeDoBooks", 
+            targets: ["WeDoBooksWrapper"]
         )
     ],
     targets: [
+        .target(
+            name: "WeDoBooksWrapper",
+            dependencies: [
+                "WeDoBooksSDK",
+                "ColibrioReader"
+            ]
+        ),
         .binaryTarget(
             name: "WeDoBooksSDK",
             url: "https://wdb-ios-spm-844218222632.europe-west1.run.app/WeDoBooksSDK-v0.1.4.xcframework.zip",
             checksum: "05d86d10825ba11911dbf80409d23f1763b40b42705dd3764698c022dca8594b"
+        ),
+        .binaryTarget(
+            name: "ColibrioReader",
+            url: "https://wdb-ios-spm-844218222632.europe-west1.run.app/ColibrioReader-v4.3.1.xcframework.zip",
+            checksum: "a35f5508e67f524b9998ec9ca3660ff918e7e7f4db9d65f5ab211e6aa12c7133"
         )
     ]
 )
