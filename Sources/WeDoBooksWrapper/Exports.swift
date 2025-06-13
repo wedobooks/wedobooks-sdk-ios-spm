e5d65a28acde4c3952a293ae841cdca9c883c847
Bo Gosmer <bo.gosmer@wedobooks.io>
2025-06-13 13:33:15 +0200

    Added comment to Export.swift

--- Sources/WeDoBooksWrapper/Exports.swift
+++ Sources/WeDoBooksWrapper/Exports.swift
@@ -1,1 +1,4 @@
+// Re-export of public WeDoBooksSDK symbols
+// NOTE: @_exported is an internal Swift attribute but widely used in SPM modules.
+// If it becomes deprecated, replace with manual wrapper API.
 @_exported import WeDoBooksSDK
