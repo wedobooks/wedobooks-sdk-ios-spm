5c68672daf2f1f415045976d2ef7d703b13d5500
Bo Gosmer <bo.gosmer@wedobooks.io>
2025-10-17 12:14:38 +0200

    Added missing import

--- Sources/WeDoBooksWrapper/EnsureFirebaseProvider.swift
+++ Sources/WeDoBooksWrapper/EnsureFirebaseProvider.swift
@@ -1,4 +1,5 @@
 import Foundation
+import FirebaseProvider
 
 // Touching FirebaseAdapterFactory here to make sure it's loaded
 // to Objective C runtime which WeDoBooksFacade.shared.setup(...) uses.
