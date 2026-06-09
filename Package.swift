9dd195b9992a43609506385cbae7cda0236ed25f
krishbee <kristoffer.frank@hotmail.com>
2026-06-09 10:56:50 +0200

    using firebase provider version 1.1.3

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/wedobooks/wedobooks-sdk-ios-firebase-provider.git", from: "1.1.2"),
+        .package(url: "https://github.com/wedobooks/wedobooks-sdk-ios-firebase-provider.git", from: "1.1.3"),
     ],
     targets: [
         .target(
