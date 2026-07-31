fce98f046b13fba871cdaf68ff01467d8a8cf019
Martin An Vo <martin.vo@wedobooks.io>
2026-07-31 11:05:38 +0200

    update to 1.7.0

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "WeDoBooksSDK",
-            url: "https://wdb-ios-spm-844218222632.europe-west1.run.app/WeDoBooksSDK-v1.6.0.xcframework.zip",
-            checksum: "93ed355d0f52f34f1a29423e555f71e0cb5aeaed9db3d18a497500852ad5171d"
+            url: "https://wdb-ios-spm-844218222632.europe-west1.run.app/WeDoBooksSDK-v1.7.0.xcframework.zip",
+            checksum: "a4fb0031df30cb53360a75cb00d6cab41e1e8c57a9d41cc0037a1d78684456d3"
         ),
         .binaryTarget(
             name: "ColibrioReader",
